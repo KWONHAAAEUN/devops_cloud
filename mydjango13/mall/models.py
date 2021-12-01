dc1984d094a5db9db9bb7d217f66dbdc94e9f327
kwonhaeun <wooyoung_000@naver.com>
2021-12-01 10:59:24 +0900

    requirements.txt 파일 추가

--- mydjango13/mall/models.py
+++ mydjango13/mall/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Shop(models.Medel):
+    name=models.CharField(max_length=100)
+    description=models.TextField(black=True)
+    photo=models.ImageField(black=True)
