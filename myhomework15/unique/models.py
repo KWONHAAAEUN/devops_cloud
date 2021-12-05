d2a9f6fbaa8fc4d2c0854067dcb5fdfa4a22638e
kwonhaeun <wooyoung_000@naver.com>
2021-12-05 20:29:00 +0900

    html 파일 생성과 medels 작성

--- myhomework15/unique/models.py
+++ myhomework15/unique/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Unique(models.Model):
+    name=models.CharField(max_length=100, db_index=True)
+    address=models.CharField(max_length=100)
+    telephone=models.CharField(max_length=15)
+    description=models.TextField(blank=True)
