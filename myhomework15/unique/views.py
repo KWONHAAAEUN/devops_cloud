448205e8be899e8fbe03765efdadd5782c31b574
kwonhaeun <wooyoung_000@naver.com>
2021-12-05 20:37:28 +0900

    views 작성

--- myhomework15/unique/views.py
+++ myhomework15/unique/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse,HttpRequest
+from unique.models import Unique
 
-# Create your views here.
+def unique_list(request: HttpRequest) -> HttpResponse:
+    qs=Unique.objects.all()
+    query=request.GET.get("query","")
+    if query:
+        qs=qs.filter(name__icontains=query)
+    context_data={
+        "unique_list":qs,
+        }
+    return  render(request,"unique/unique_list.html",context_data)
+
+def unique_detail(request: HttpRequest, pk:int) -> HttpResponse:
+    unique=Unique.objects.get(pk=pk)
+    context_data={
+        "unique":unique,
+    }
+    return render(request,"unique/unique_detail.html",context_data)
