3dff04b6e23d36cdaa4d37c2bcb7331e9532f430
kwonhaeun <wooyoung_000@naver.com>
2021-12-07 14:17:23 +0900

    간단 검색하는 form 작성

--- mydjango17/diary/views.py
+++ mydjango17/diary/views.py
@@ -4,6 +4,16 @@ from diary.models import Post
 
 def post_list(request: HttpRequest)->HttpResponse:
     qs=Post.objects.all() # 전체 포스팅 목록을 얻어올 준!비!
+    query=request.GET.get("query","")
+    if query: # 검색어가 있다면
+        qs=qs.filter(title__icontains=query)
+
     return render(request,"diary/post_list.html",{
         "post_list":qs,
+    })
+
+def post_detail(request:HttpRequest, pk:int) -> HttpResponse:
+    post=Post.objects.get(pk=pk)
+    return render(request,"diary/post_detail.html",{
+        "post":post,
     })
