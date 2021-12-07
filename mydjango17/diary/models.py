8afa5a83179a48275680b9e64287abec1e7e9605
kwonhaeun <wooyoung_000@naver.com>
2021-12-07 11:02:57 +0900

    models, migrations 생성 및 admin에 등록

--- mydjango17/diary/models.py
+++ mydjango17/diary/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class TimestampedModel(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True # 추상 클래스로서, DB 테이블이 생기지 않는다다
+
+#db_index는 db에 인덱스를 생성시켜 조건을 넣으면 검색이 가능하게 하는 부분
+class Post(TimestampedModel):
+    author_name=models.CharField(max_length=20)
+    title=models.CharField(max_length=200, db_index=True)
+    content=models.TextField()
+    photo=models.ImageField(upload_to="diary/post/%Y/%m/%d")
+    # 그냥 Tag를 적으면 Tag가 아래에 있기에 받지 못한다
+    # 문자열로 넣어주면 현재 앱에서 Tag를 찾아 받아준다
+    tag_set=models.ManyToManyField('Tag',blank=True)
+
+    def __str__(self)->str:
+        return self.title
+
+    class Meta:
+        # 단수
+        verbose_name="포스팅"
+        # 복수
+        verbose_name_plural="포스팅 목록"
+
+class Comment(TimestampedModel):
+    author_name=models.CharField(max_length=20)
+    message=models.TextField()
+
+    class Meta:
+        verbose_name="댓글"
+        verbose_name_plural="댓글 목록"
+
+class Tag(TimestampedModel):
+    name=models.CharField(max_length=200, db_index=True)
+
+    def __str__(self)->str:
+        return self.name
+
+    class Meta:
+        verbose_name="태그"
+        verbose_name_plural="태그 목록"
+
+# class에 TimestampedModel를 넣음으로 상속을 하는 것
