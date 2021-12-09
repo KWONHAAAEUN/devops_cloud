fccb6f6fbf1f39a25f666fee76dd98f6f041d8c5
kwonhaeun <wooyoung_000@naver.com>
2021-12-09 13:39:17 +0900

    form 작성 페이지 만들고 오류 예상

--- mydjango19/diary/urls.py
+++ mydjango19/diary/urls.py
@@ -6,6 +6,7 @@ app_name="diary"
 
 urlpatterns=[
     path("",views.post_list, name="post_list"),
-    path("<int:pk>/",views.post_detail,name="post_detail"), # 주소 매칭
+    path("<int:pk>/",views.post_detail,name="post_detail"),# 주소 매칭
+    path("new/",views.post_new,name="post_new"),
     path("tags/<str:tag_name>/",views.tag_detail,name="tag_detail"),
 ]
