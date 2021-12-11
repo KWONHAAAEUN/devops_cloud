9924c86549f9e1e1897e3f9ccd6c9307785c1255
kwonhaeun <wooyoung_000@naver.com>
2021-12-12 02:59:07 +0900

    tag와 review를 detail에 보여주기

--- myhomework20/car/urls.py
+++ myhomework20/car/urls.py
@@ -6,4 +6,5 @@ app_name="car"
 urlpatterns=[
     path("",views.shop_list,name="shop_lost"),
     path("<int:pk>/",views.shop_detail,name="shop_detail"),
+    path("tags/<str:tag_name>/",views.tag_detail,name="tag_detail"),
 ]
--- myhomework20/car/views.py
+++ myhomework20/car/views.py
@@ -3,15 +3,30 @@ from django.shortcuts import render, get_object_or_404
 
 from car.models import Shop
 
-
 def shop_list(request:HttpRequest)->HttpResponse:
     qs=Shop.objects.all()
+    query = request.GET.get("query", "")
+    if query:  # 검색어가 있다면
+        qs = qs.filter(name__icontains=query)
     return render(request,"car/shop_list.html",{
         "shop_list":qs,
     })
 
-def shop_detail(request:HttpRequest, pk:int)->HttpResponse:
+def shop_detail(request:HttpRequest,pk:int)->HttpResponse:
     shop=get_object_or_404(Shop,pk=pk)
+    tag_list=shop.tag_set.all()
+    review_list = shop.review_set.all()
     return render(request,"car/shop_detail.html",{
         "shop":shop,
+        "tag_list":tag_list,
+        "review_list": review_list,
     })
+
+def tag_detail(request:HttpRequest,tag_name:str)->HttpResponse:
+    qs=Shop.objects.all()
+    qs=qs.filter(tag_set__name=tag_name)
+    return render(request,"car/tag_detail.html",{
+        "tag_name": tag_name,
+        "post_list":qs,
+    })
+
