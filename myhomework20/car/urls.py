a01a94f83ac08fb29d7657eb605dbc0331a4d405
kwonhaeun <wooyoung_000@naver.com>
2021-12-12 04:05:12 +0900

    헤더를 클릭시에 root로 이동

--- myhomework20/car/urls.py
+++ myhomework20/car/urls.py
@@ -4,7 +4,9 @@ from car import views
 app_name="car"
 
 urlpatterns=[
-    path("",views.shop_list,name="shop_lost"),
+    path("",views.shop_list,name="shop_list"),
     path("<int:pk>/",views.shop_detail,name="shop_detail"),
     path("tags/<str:tag_name>/",views.tag_detail,name="tag_detail"),
+    path("<int:shop_pk>/reviews/new",views.review_new,name="review_new"),
+    path("<int:shop_pk>/reviews/<int:pk>/edit/",views.review_edit,name="review_edit"),
 ]
