11f7b975dff6276a92d06b60c3a083ac0f8cdfed
kwonhaeun <wooyoung_000@naver.com>
2021-12-13 21:28:00 +0900

    tag를 보이게 하기

--- myhomework21_1/volleyball/views.py
+++ myhomework21_1/volleyball/views.py
@@ -3,9 +3,19 @@ from django.shortcuts import render, get_object_or_404
 
 from volleyball.models import Player
 
+def tag_detail(request: HttpRequest, tag_name:str)->HttpResponse:
+    qs=Player.objects.all()
+    qs=qs.filter(tag_set__name=tag_name)
+    return render(request,"volleyball/tag_detail.html",{
+        "tag_name": tag_name,
+        "player_list":qs,
+    })
 
 def player_list(request:HttpRequest)->HttpResponse:
     qs=Player.objects.all()
+    query=request.GET.get("query","")
+    if query: # 검색어가 있다면
+        qs=qs.filter(name__icontains=query)
     return render(request,"volleyball/player_list.html",{
         "player_list":qs,
     })
