c203f1a29fe874db78c0ce8296cee53acc66ad39
kwonhaeun <wooyoung_000@naver.com>
2021-12-13 18:05:13 +0900

    .gitignore에 venv 추가와 admin 작성

--- myhomework21_1/volleyball/admin.py
+++ myhomework21_1/volleyball/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from volleyball.models import Category, Player, Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
