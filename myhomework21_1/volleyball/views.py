c63d04b8a72e16be08d7040e66e461ddd8ab12f4
kwonhaeun <wooyoung_000@naver.com>
2021-12-13 18:13:55 +0900

    views에 list와 detail 작성

--- myhomework21_1/volleyball/views.py
+++ myhomework21_1/volleyball/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from volleyball.models import Player
+
+
+def player_list(request:HttpRequest)->HttpResponse:
+    qs=Player.objects.all()
+    return render(request,"volleyball/player_list.html",{
+        "player_list":qs,
+    })
+
+def player_detail(request:HttpRequest,pk:int)->HttpResponse:
+    player=get_object_or_404(Player,pk=pk)
+    return render(request,"volleyball/player_detail.html",{
+        "player":player,
+    })
