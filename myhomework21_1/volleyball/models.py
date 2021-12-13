09c720ee60faa7815761619056a7678640d3796d
kwonhaeun <wooyoung_000@naver.com>
2021-12-13 17:59:11 +0900

    models.py 작성

--- myhomework21_1/volleyball/models.py
+++ myhomework21_1/volleyball/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+
+    def __str__(self)->str:
+        return self.name
+
+    class Meta:
+        ordering=["-name"]
+
+class Player(models.Model):
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    name=models.CharField(max_length=30,db_index=True)
+    photo=models.ImageField(upload_to="volleyball/player/%Y/%m/%d")
+    team=models.CharField(max_length=30)
+    position=models.CharField(max_length=30)
+    description=models.TextField(blank=True)
+    tag_set=models.ManyToManyField('Tag',blank=True)
+
+    def __str__(self)->str:
+        return self.name
+
+    class Meta:
+        ordering=["-name"]
+
+class Tag(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+
+    def __str__(self)->str:
+        return self.name
+
+    class Meta:
+        ordering=["-name"]
+
