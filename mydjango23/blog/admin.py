de9691cf3b124baf5a6eafbf8c00f81a0a777e51
kwonhaeun <wooyoung_000@naver.com>
2021-12-15 11:18:13 +0900

    models작성 및 admin에 등록

--- mydjango23/blog/admin.py
+++ mydjango23/blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Category, Post, Tag, Comment
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Post)
+class PlayerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
