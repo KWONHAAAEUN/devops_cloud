4ffe5a44f94eed1452c7b6b22ac5491ec3000dff
kwonhaeun <wooyoung_000@naver.com>
2021-12-16 22:09:06 +0900

    models 작성하고 admin설정

--- myhomework24_1/star/admin.py
+++ myhomework24_1/star/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from star.models import Category, Kirby, Tag, Review
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Kirby)
+class KirbyAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
--- myhomework24_1/star/models.py
+++ myhomework24_1/star/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Category(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+class Kirby(TimestampedModel):
+    title=models.CharField(max_length=30,db_index=True)
+    photo=models.ImageField(upload_to="star/kirby/%Y/%m/%d")
+    day=models.DateField()
+    story=models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+class Review(TimestampedModel):
+    kirby = models.ForeignKey(Kirby, on_delete=models.CASCADE)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        ordering = ['-id']
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
