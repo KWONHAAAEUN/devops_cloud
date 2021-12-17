f697afabaf698fd328b1113e22aca8edfbe3e240
kwonhaeun <wooyoung_000@naver.com>
2021-12-17 14:20:17 +0900

    shop_list작성

--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from shop.models import Shop, Category
+
+
+class ShopListView(ListView):
+    model = Shop
+
+    # 부모의 get context date를 불러와서 저장
+    def get_context_data(self, **kwargs):
+        context_date=super().get_context_data(**kwargs)
+        context_date["category_list"]=Category.objects.all()
+        return context_date
+
+shop_list=ShopListView.as_view()
