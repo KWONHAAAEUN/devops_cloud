6959d620cbfc085e0ad6241fe87ca112b1f9ff8a
kwonhaeun <wooyoung_000@naver.com>
2021-12-17 10:49:27 +0900

    accounts 앱 추가 생성

--- mydjango25/mydjango25/urls.py
+++ mydjango25/mydjango25/urls.py
@@ -6,7 +6,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',TemplateView.as_view(template_name='root.html'),name="root")
+    path('',TemplateView.as_view(template_name='root.html'),name="root"),
+    path('shop/',include('shop.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
