7ae5689f088fdc9eacb093997f50e36ea6b96d4e
kwonhaeun <wooyoung_000@naver.com>
2021-12-17 15:37:39 +0900

    리뷰 작성 구현

--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
-from shop.models import Shop, Category
+from shop.forms import ReviewForm
+from shop.models import Shop, Category, Review
 
 
 class ShopListView(ListView):
@@ -9,12 +13,36 @@ class ShopListView(ListView):
 
     # 부모의 get context date를 불러와서 저장
     def get_context_data(self, **kwargs):
-        context_date=super().get_context_data(**kwargs)
-        context_date["category_list"]=Category.objects.all()
+        context_date = super().get_context_data(**kwargs)
+        context_date["category_list"] = Category.objects.all()
         return context_date
 
-shop_list=ShopListView.as_view()
 
-shop_detail=DetailView.as_view(
+shop_list = ShopListView.as_view()
+
+shop_detail = DetailView.as_view(
     model=Shop,
 )
+
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    model = Review
+    form_class = ReviewForm
+    # FIXME: shop_detail로 이동
+    success_url = reverse_lazy("shop:shop_list")
+
+    # 유효성 검사에 통과한다면 ..
+    def form_valid(self, form) -> HttpResponse:
+        # self.kwargs:URL Captured 값들이 사전으로 저장
+        shop_pk = self.kwargs["shop_pk"]
+        shop = get_object_or_404(Shop, pk=shop_pk)
+
+        review = form.save(commit=False)
+        review.shop = shop
+        review.user = self.request.user
+        review.save()
+        # return redirect("shop:shop_detail",shop.pk)
+        return redirect(shop)
+
+
+review_new = ReviewCreateView.as_view()
