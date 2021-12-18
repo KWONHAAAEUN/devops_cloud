571c34439b885879670cb81a42797ffb90dbfd8d
kwonhaeun <wooyoung_000@naver.com>
2021-12-18 20:45:55 +0900

    admin 작성

--- myhomework25/ani/admin.py
+++ myhomework25/ani/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from ani.models import Category, Ani, Comment, Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Ani)
+class AniAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
