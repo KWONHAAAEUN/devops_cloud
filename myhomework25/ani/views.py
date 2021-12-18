41009154d9f060a412eab5a502e05ecf1488942b
kwonhaeun <wooyoung_000@naver.com>
2021-12-18 21:00:13 +0900

    category 보이게 설정

--- myhomework25/ani/views.py
+++ myhomework25/ani/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from ani.models import Ani
+from ani.models import Ani, Category
 
-ani_list=ListView.as_view(
-    model=Ani,
-)
+
+class AniListView(ListView):
+    model=Ani
+
+    def get_context_data(self,**kwargs):
+        context_date=super().get_context_data(**kwargs)
+        context_date["category_list"]=Category.objects.all()
+        return context_date
+
+ani_list=AniListView.as_view()
