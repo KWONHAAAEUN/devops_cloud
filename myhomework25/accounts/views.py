4a60edc2faa2c281a6af148552494b75d033a32a
kwonhaeun <wooyoung_000@naver.com>
2021-12-18 20:09:21 +0900

    login,profile,signup,logout 구현

--- myhomework25/accounts/views.py
+++ myhomework25/accounts/views.py
@@ -1,13 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
-def signup(request):
-    pass
+signup=CreateView.as_view(
+    form_class=UserCreationForm,
+    success_url=reverse_lazy("accounts:login"),
+    template_name="accounts/signup_form.html",
+)
 
-def login(request):
-    pass
+login=LoginView.as_view(
+    template_name="accounts/login_form.html",
+)
 
-def profile(request):
-    pass
+profile=login_required(TemplateView.as_view(
+    template_name="accounts/profile.html"
+))
 
-def logout(request):
-    pass
+logout=LogoutView.as_view(
+    next_page="root",
+)
