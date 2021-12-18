1f5e92da212326068a11346f9da8ab45be8149c7
kwonhaeun <wooyoung_000@naver.com>
2021-12-18 20:31:10 +0900

    Ani app models 작성

--- myhomework25/ani/models.py
+++ myhomework25/ani/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+class TimestampedModel(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+
+class Category(TimestampedModel):
+    name=models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering=["name"]
+        verbose_name="카테고리"
+        verbose_name_plural="카테고리들"
+
+class Ani(TimestampedModel):
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    name=models.CharField(max_length=30,db_index=True)
+    photo=models.ImageField(upload_to="ani/ani/%Y/%m/%d",blank=True)
+    description=models.TextField(blank=True)
+    tag_set=models.ManyToManyField('Tag',blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering=["name"]
+        verbose_name="캐릭터"
+        verbose_name_plural="캐릭터 이름들"
+
+class Comment(TimestampedModel):
+    ani=models.ForeignKey(Ani,on_delete=models.CASCADE)
+    user=models.CharField(max_length=30,db_index=True)
+    message=models.TextField()
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        ordering=["-id"]
+        verbose_name="댓글"
+        verbose_name_plural="댓글 목록"
+
+class Tag(TimestampedModel):
+    name=models.CharField(max_length=100,unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering=["name"]
+        verbose_name="태그"
+        verbose_name_plural="태그 목록"
