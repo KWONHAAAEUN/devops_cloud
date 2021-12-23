71017ec9aa94be94a7b46ecc997b0bae527a834e
kwonhaeun <wooyoung_000@naver.com>
2021-12-23 13:20:46 +0900

    Counter

--- myreact29/src/Counter.jsx
+++ myreact29/src/Counter.jsx
@@ -27,15 +27,4 @@ function Counter() {
   );
 }
 
-function App() {
-  return (
-    <div>
-      <h1>안녕 리액트</h1>
-      <Counter />
-      <Counter />
-      <Counter />
-    </div>
-  );
-}
-
-export default App;
+export default Counter;
