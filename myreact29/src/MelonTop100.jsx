8d2e9cdadfe421372998664d1c79811e47c50ef7
kwonhaeun <wooyoung_000@naver.com>
2021-12-23 15:23:23 +0900

    api서버에 axios 호출을 통해 구현

--- myreact29/src/MelonTop100.jsx
+++ myreact29/src/MelonTop100.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Button as BootstrapButton } from 'react-bootstrap';
-import { Button as AntButton } from 'antd';
+import { Button as AntdButton } from 'antd';
+import Axios from 'axios';
 import initialsongList from './data/melon_data.json';
 import './MelonTop100.css';
 
@@ -10,20 +11,44 @@ import './MelonTop100.css';
 function MelonTop100() {
   const [songList, setSongList] = useState([]);
 
-  const handleClick = () => {
+  const handleClick1 = () => {
     setSongList(initialsongList);
+  };
+
+  const handleClick2 = () => {
+    const url = 'https://antigravity-daejeon-2021.herokuapp.com/api/melon/';
+    Axios.get(url)
+      .then((response) => {
+        const { data } = response;
+        setSongList(data);
+        // console.log('응답을 받았습니다.');
+        // console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleClick3 = () => {
+    setSongList([]);
   };
 
   return (
     <div>
       <h1>멜론 top 100</h1>
-      <BootstrapButton variant="success" onClick={handleClick}>
-        로딩
+      <p>코딩요정 다녀감ㅋ</p>
+      <BootstrapButton variant="success" onClick={handleClick1}>
+        파일 로딩
       </BootstrapButton>
 
-      <AntButton type="primary" onClick={handleClick}>
-        로딩
-      </AntButton>
+      <AntdButton type="primary" onClick={handleClick2}>
+        서버 로딩
+      </AntdButton>
+
+      <AntdButton type="dashed" onClick={handleClick3}>
+        클리어
+      </AntdButton>
+
       <ul className="songList">
         <ul>
           {songList.map((song) => {
