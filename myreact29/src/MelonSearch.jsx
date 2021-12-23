ff155d62990f795d00de6c4f07a4d47eb3f6aaca
kwonhaeun <wooyoung_000@naver.com>
2021-12-23 17:57:36 +0900

    search에 list 디자인 적용

--- myreact29/src/MelonSearch.jsx
+++ myreact29/src/MelonSearch.jsx
@@ -1,8 +1,12 @@
 import { Input } from 'antd';
 import { useState } from 'react';
+import { List, Avatar } from 'antd';
+import Axios from 'axios';
+import jsonpAdapter from 'axios-jsonp';
 
 function MelonSearch() {
   const [query, setQuery] = useState('');
+  const [songList, setSongList] = useState([]);
 
   const handleChange = (e) => {
     const {
@@ -17,7 +21,40 @@ function MelonSearch() {
     console.group('handlePressEnter');
     console.log(`검색어 ${query}로 검색합니다`);
     console.groupEnd();
+
+    const url = 'https://www.melon.com/search/keyword/index.json';
+
+    Axios({
+      url: url,
+      adapter: jsonpAdapter,
+      callbackParamName: 'jscallback',
+      params: {
+        query: query,
+      },
+    })
+      .then((response) => {
+        const {
+          data: { SONGCONTENTS: searchedSongList },
+        } = response;
+
+        console.group('멜론 검색 결과');
+        console.log(response);
+        console.log(searchedSongList);
+        console.groupEnd();
+
+        setSongList(searchedSongList);
+      })
+      .catch((error) => {
+        console.group('멜론 검색 에러');
+        console.log(error);
+        console.groupEnd();
+      });
   };
+  const data = [
+    {
+      title: '',
+    },
+  ];
 
   return (
     <div style={{ width: 300, margin: '0 auto' }}>
@@ -28,6 +65,34 @@ function MelonSearch() {
         onChange={handleChange}
         onPressEnter={handlePressEnter}
       />
+      {songList.map((song) => {
+        return (
+          <List
+            itemLayout="horizontal"
+            dataSource={data}
+            renderItem={() => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Avatar src={song.ALBUMIMG} />}
+                  title={song.SONGNAME}
+                  description={song.ARTISTNAME}
+                />
+              </List.Item>
+            )}
+          />
+        );
+      })}
+      {/* <List>
+        {songList.map((song) => {
+          return (
+            <div>
+              const data=[ ]
+              <img src={song.ALBUMIMG} />
+              {song.SONGNAME} by {song.ARTISTNAME}
+            </div>
+          );
+        })}{' '}
+      </List> */}
     </div>
   );
 }
