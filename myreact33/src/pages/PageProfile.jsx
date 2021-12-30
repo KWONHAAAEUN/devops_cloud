fa79df14f1ed20e147204497e6b834cd2fa6a40a
kwonhaeun <wooyoung_000@naver.com>
2021-12-30 15:57:15 +0900

    3단계 스테이지8 검색기능

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -5,7 +5,10 @@ const { useEffect } = require('react');
 
 function PageProfile() {
   const [profileList, setProfileList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
   const [error, setError] = useState(null);
+  const [query, setQuery] = useState('');
+
   const handleRefresh = () => {
     setError(null); // setError의 값을 null로 변경하여 오류가 저장되었던 것을 비워줌
     Axios.get(
@@ -21,6 +24,7 @@ function PageProfile() {
           profileImageUrl: profile.profile_image_url,
         }));
         setProfileList(newNameList);
+        setFilteredList(newNameList);
       })
       // reponse는 axios 객체
       // response.data => 응답 내용
@@ -28,25 +32,57 @@ function PageProfile() {
         setError(error); // error가 생기면 setError에 값을 저장함
       });
   };
-  const [userNum, setUserNum] = useState('bts-jin');
-
-  const handleClear = () => {
-    setProfileList([]);
-  };
 
   useEffect(() => handleRefresh(), []);
+
+  const [userNum, setUserNum] = useState('bts-jin');
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    console.log(value);
+    setQuery(value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      // Enter 키를 입력했습니다.
+      console.log('ENTER');
+      // const value = e.target.value;
+      // setQuery(value);
+      const profile1 = profileList.filter((member) => {
+        if (query.length === 0) {
+        }
+        return (
+          member.name.includes(query) ||
+          member.role.includes(query) ||
+          member.mbti.includes(query)
+        );
+      });
+      setFilteredList(profile1);
+    }
+  };
+
+  const handleClear = () => {
+    setFilteredList([]);
+  };
 
   return (
     <div>
       <h1>Profile List</h1>
+      <input
+        type="text"
+        placeholder="검색어를 입력해주세요."
+        onKeyPress={handleKeyPress}
+        onChange={handleChange}
+      />
       <button onClick={handleClear}>클리어</button>
       <button onClick={handleRefresh}>새로고침</button>
-      {profileList.length === 0 && <h3>등록된 프로필이 없습니다</h3>}
+      {filteredList.length === 0 && <h3>등록된 프로필이 없습니다</h3>}
       {error && (
         <h3>조회 시에 오류가 발생했습니다. 잠시 후 다시 시도해주세요</h3>
       )}
       {/* 선택적 랜더링을 통해 error가 발생하면 메세지를 내보냄 */}
-      {profileList.map((bts) => {
+      {filteredList.map((bts) => {
         return (
           <div key={bts.uniqueId}>
             <h2>{bts.uniqueId}</h2>
